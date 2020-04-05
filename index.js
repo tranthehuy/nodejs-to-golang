99e5153e55020e61cf3926a16502be17ad474fb1
johnny <huytran@webenabled.com>
2020-04-05 18:42:14 +0700

    detect console log

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ fs.readFile('./source/index.js', 'utf8', function(err, program) {
   if (err) console.log('File not found');
   console.log('program', program);
   const astTree = esprima.parseScript(program);
-  utils.convertToFile(astTree, './dest/main.go');
+  const sourceCode = utils.convertToFile(astTree, './dest/main.go');
   const astStr = JSON.stringify(astTree, null, "  ");
-  console.log(astStr);
+  console.log('Tree: \n', astStr);
+  console.log('SourceCode: \n', sourceCode);
 });
 
--- utils.js
+++ utils.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+const _ = require('lodash');
+
 const TYPES = {
-  PROGRAM: 'Program'
+  Program: 'Program',
+  VariableDeclaration: 'VariableDeclaration',
+  ExpressionStatement: 'ExpressionStatement',
+  CallExpression: 'CallExpression',
 }
 
 const ERRORS = {
@@ -10,15 +16,57 @@ const logger = {
   info: console.log
 }
 
-const convertToFile  = (astTree, outputFile) => {
-  const outputCode = [];
-  if (astTree.type !== TYPES.PROGRAM) {
+const convertElementToLine = (element) => {
+  let result = '';
+  switch (element && element.type) {
+    case TYPES.ExpressionStatement: 
+      const objectName = _.get(element, 'expression.callee.object.name', '');
+      const propertyName = _.get(element, 'expression.callee.property.name', '');
+      if (objectName === 'console' && propertyName === 'log') {
+        const argument0 = _.get(element, 'expression.arguments[0].value', '');
+        result = `fmt.Println("${argument0}")`;
+      }
+      break;      
+  }
+  return result;
+};
+
+const convertProgramElementToCode = (element) => {
+  if (element.type !== TYPES.Program) {
     logger.info(ERRORS.wrongCode);
     return;
   }
 
+  const elements = element.body;
+  const lines = elements.map(e => convertElementToLine(e));
+  lines.unshift('  // main code here');
+  const sourceCode = lines.join("\n  ");
+
+  const result = `
+package main
+import "fmt"
+func main() {
+${sourceCode}
+}
+  `;
+
+  return result;
 }
 
-export default {
+const convertToFile  = (astTree, outputFile) => {
+  let outputCode = "// created by Johnny Chen tools\n";
+  
+  const sourceCode = convertProgramElementToCode(astTree);
+  outputCode += sourceCode;
+
+  fs.writeFile(outputFile, outputCode, function (err) {
+    if (err) throw err;
+    console.log('Write output file done!');
+  });
+
+  return outputCode;
+}
+
+module.exports = {
   convertToFile
 }
